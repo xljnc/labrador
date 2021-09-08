452bf33951ca989c1fefb2de6a78608f940ec6b5
qiyu <jsj0804wt@126.com>
2021-09-08 23:03:17 +0800

    init project by qiyu 2021-09-08

---
+++ src/main/java/com/wt/labrador/LabradorApplication.java
@@ -0,0 +1,15 @@
+package com.wt.labrador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 一贫
+ * @date 2021/8/19
+ */
+@SpringBootApplication
+public class LabradorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LabradorApplication.class, args);
+    }
+}
